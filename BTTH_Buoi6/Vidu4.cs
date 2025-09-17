38a4b8c1b412938af7ecfb7a9d334963dff16d49
Van1912 <nkv191219@gmail.com>
2025-09-17 19:38:59 +0700

    Add BTTH_Buoi6 folder

---
+++ BTTH_Buoi6/Vidu4.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTTH_Buoi6
+{
+	internal class Vidu4
+	{
+		static void Main(string[] args)
+		{
+			Console.InputEncoding = Encoding.UTF8;
+			Console.OutputEncoding = Encoding.UTF8;
+			Console.WriteLine("Nhập số N:");
+			int N = int.Parse(Console.ReadLine());
+
+			if (N <= 0)
+			{
+				Console.WriteLine("N phải lớn hơn 0");
+				return;
+			}
+
+			int[] numbers = new int[N];
+			for (int i = 0; i < N; i++)
+			{
+				Console.WriteLine($"Nhập số thứ {i+1}:");
+				numbers[i] = int.Parse(Console.ReadLine());
+			}
+			int max = numbers[0];
+			for (int i = 1; i < N; i++)
+			{
+				if (numbers[i] > max)
+				{
+					max = numbers[i];
+				}
+			}
+            Console.WriteLine($"Số lớn nhất là {max}");	
+			} 		
+		}
+	}
+
---
+++ Bai1/Bai1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai1
+{
+	internal class Bai1
+	{
+		static void Main(string[] args)
+		{
+			Console.InputEncoding = Encoding.UTF8;
+			Console.OutputEncoding = Encoding.UTF8;
+			Console.Write("Nhập N:");
+			int N;
+			while(!int.TryParse(Console.ReadLine(), out N) && N>0)
+			{
+				Console.WriteLine("Nhập lại N:");
+			}	
+
+			int[] numbers = new int[N];
+			//Nhập các phần tử của mảng
+			for (int i = 0; i < N; i++)
+			{
+				Console.Write($"Nhập phần tử {i + 1}:");
+				numbers[i] = int.Parse(Console.ReadLine());
+			}
+			//Nhập số cần kiểm tra
+			Console.Write("Nhập số nguyên x:");
+			int x = int.Parse(Console.ReadLine());
+			//Kiểm tra x có trong mảng hay không
+			bool tìm = false;
+			for (int i = 0; i < N; i++)
+			{
+				if (numbers[i] == x)
+				{
+					tìm = true;
+					break;
+				}
+			}
+			if (tìm)
+			{
+				Console.WriteLine($"Số {x} có trong mảng");
+			}
+			else Console.WriteLine($"Số {x} không có trong mảng");	
+		}
+	}
+}
